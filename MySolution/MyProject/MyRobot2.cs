ffc46943fc405bac5cf7fd1467dcddac7c85506a
dxbgithub <ddz383@hotmail.com>
2016-05-25 11:06:34 +0800

    Init version

---
+++ MySolution/MyProject/MyRobot2.cs
@@ -0,0 +1,57 @@
+﻿using Robocode;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNL
+{
+    public class MyRobot2 : Robot
+    {
+        // The main method of your robot containing robot logics
+        public override void Run()
+        {
+            // -- Initialization of the robot --
+            IsAdjustGunForRobotTurn = true;
+            IsAdjustRadarForRobotTurn = true;
+            IsAdjustRadarForGunTurn = false;
+            RadarColor = Color.Yellow;
+            // Here we turn the robot to point upwards, and move the gun 90 degrees
+            TurnLeft(Heading - 90);
+            TurnGunRight(90);
+
+            // Infinite loop making sure this robot runs till the end of the battle round
+            while (true)
+            {
+                // -- Commands that are repeated forever --
+
+                // Move our robot 5000 pixels ahead
+                Ahead(180);
+
+                // Turn the robot 90 degrees
+                TurnRight(45);
+
+                TurnGunRight(180);
+
+                // Our robot will move along the borders of the battle field
+                // by repeating the above two statements.
+            }
+        }
+
+        // Robot event handler, when the robot sees another robot
+        public override void OnScannedRobot(ScannedRobotEvent e)
+        {
+            // We fire the gun with bullet power = 1
+            Fire(4);
+            Scan();
+        }
+
+        public override void OnHitByBullet(HitByBulletEvent evnt)
+        {
+            //base.OnHitByBullet(evnt);
+
+        }
+    }
+}
